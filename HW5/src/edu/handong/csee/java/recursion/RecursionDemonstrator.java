f3ce14c90c3e4b87a9cc21b6c2f0878989cb111d
hyeokkiyaa <khm38607574@gmail.com>
2024-04-28 21:59:51 +0900

    createing functions

--- HW5/src/edu/handong/csee/java/recursion/RecursionDemonstrator.java
+++ HW5/src/edu/handong/csee/java/recursion/RecursionDemonstrator.java
@@ -1,13 +1,19 @@
 package edu.handong.csee.java.recursion;
 
+import javax.swing.plaf.ComponentUI;
+
+import edu.handong.csee.java.recursion.RecursionTasks;
 
 public class RecursionDemonstrator{
-    public static void main(String args[]){
+    public static void main(String[] args){
         RecursionDemonstrator MyRecursionDemonstrator = new RecursionDemonstrator();
         MyRecursionDemonstrator.run(args); 
     }
 
     public void run(String[] args){
-        
+        RecursionTasks recursion = new RecursionTasks();
+        recursion.setTaskName(args[0].toUpperCase());
+        recursion.setInput(args);
+
     }
 }
--- HW5/src/edu/handong/csee/java/recursion/RecursionTasks.java
+++ HW5/src/edu/handong/csee/java/recursion/RecursionTasks.java
@@ -1,5 +1,94 @@
 package edu.handong.csee.java.recursion;
 
 public class RecursionTasks {
-    
+    private String taskName;
+    private String[] input;
+    private int inputForIntegerFunction = 0;
+    private String result;
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public void setInput(String[] inputMain) {
+        setTaskName(inputMain[0]);
+        switch (taskName) {
+            case "FACTORIAL":
+            case "BUNNYEARS":
+            case "DECIMAL2BINARY":
+                
+                inputForIntegerFunction = Integer.parseInt(inputMain[1]);
+                break;
+
+            case "COUNTHI":
+            case "CHANGEPI":
+            case "ENDX":
+            case "STRINGCLEAN":
+            case "DECIDESTRUBG":
+            case "PARSEBOOLEXPR":
+                input[0] = inputMain[1];
+                break;
+
+            case "STRDIST":
+                input[0] = inputMain[1];
+                input[1] = inputMain[2];
+                break;
+            
+            default:
+                System.out.println("Function name does not exist");
+                System.exit(0);
+        }
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public String[] getInput() {
+        return input;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void compute(){
+        switch (taskName) {
+            case "FACTORIAL":
+                result = Integer.toString(factorial(inputForIntegerFunction));
+            case "BUNNYEARS":
+            case "DECIMAL2BINARY":
+
+                inputForIntegerFunction = Integer.parseInt(inputMain[1]);
+                break;
+
+            case "COUNTHI":
+            case "CHANGEPI":
+            case "ENDX":
+            case "STRINGCLEAN":
+            case "DECIDESTRUBG":
+            case "PARSEBOOLEXPR":
+                input[0] = inputMain[1];
+                break;
+
+            case "STRDIST":
+                input[0] = inputMain[1];
+                input[1] = inputMain[2];
+                break;
+
+            default:
+                System.out.println("Function name does not exist");
+                System.exit(0);
+        }
+        inputForIntegerFunction = 0;
+    }
+
+    public int factorial(int num) {
+
+        return 0;
+    }
 }
