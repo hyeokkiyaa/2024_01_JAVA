b8870a28adf0c347fc9cc0d689b12f57d32cf9d7
hyeokkiyaa <khm38607574@gmail.com>
2024-04-28 20:53:06 +0900

    create ppts and new assignments HW5

---
+++ HW5/src/edu/handong/csee/java/recursion/RecursionDemonstrator.java
@@ -0,0 +1,13 @@
+package edu.handong.csee.java.recursion;
+
+
+public class RecursionDemonstrator{
+    public static void main(String args[]){
+        RecursionDemonstrator MyRecursionDemonstrator = new RecursionDemonstrator();
+        MyRecursionDemonstrator.run(args); 
+    }
+
+    public void run(String[] args){
+        
+    }
+}
