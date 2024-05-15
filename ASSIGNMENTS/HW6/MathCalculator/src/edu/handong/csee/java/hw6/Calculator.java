f4cac8cbbc75ca668ee8e9ec2e1d82ce8090e1a2
hyeokkiyaa <khm38607574@gmail.com>
2024-05-16 01:28:34 +0900

    CHANGED MADE IN INVALIDCOMMANDEXCEPTION AND CALCULATOR

--- ASSIGNMENTS/HW6/MathCalculator/src/edu/handong/csee/java/hw6/Calculator.java
+++ ASSIGNMENTS/HW6/MathCalculator/src/edu/handong/csee/java/hw6/Calculator.java
@@ -1,6 +1,7 @@
 package edu.handong.csee.java.hw6;
 
 import edu.handong.csee.java.hw6.engines.*;
+import edu.handong.csee.java.hw6.exceptions.InvalidCommandException;
 
 /**
  * A class to perform various types of calculations.
@@ -34,57 +35,60 @@ public class Calculator {
      * @param args The command-line arguments passed when the program starts.
      */
     public void run(String[] args) {
-        if(args.length==0){
-            System.out.println("Please put a computing engine option such as LCM, GCD, SQRT, FACTORIAL, FIBONACCI, MAX, MIN, CUBEVOL, and SPHEREVOL.");
-            System.out.println("For example, java -cp [your classpath] edu.handong.csee.java.hw4.Calculator MAX 12 4 5 45 100");
-            System.exit(0); 
-        }
-        // Converts the first argument to uppercase to match the engine names.
-        String engineName = args[0].toUpperCase();
-
-        // Declares a variable to hold the computable engine.
-        Computable engine = null;
-
-        // Chooses the appropriate engine based on the specified engine name.
-        switch(engineName) {
-            case "LCM":
-                engine = new LCMEngine();
-                break;
-            case "GCD":
-                engine = new GCDEngine();
-                break;
-            case "SQRT":
-                engine = new SQRTEngine();
-                break;
-            case "FACTORIAL":
-                engine = new FactorialEngine();
-                break;
-            case "FIBONACCI":
-                engine = new FibonacciEngine();
-                break;
-            case "MAX":
-                engine = new MaxEngine();
-                break;
-            case "MIN":
-                engine = new MinEngine();
-                break;
-            case "CUBEVOL":
-                engine = new CubeVolEngine();
-                break;
-            case "SPHEREVOL":
-                engine = new SphereVolEngine();
-                break;
-            default:
-                System.out.println("Invalid command: " + engineName);
-                System.exit(0);
-        }
-
-        // Sets the input values for the engine.
-        engine.setInput(args);
-        // Computes the result using the specified engine.
-        engine.compute();
-
-        // Prints the result of the computation.
-        System.out.println("The result of " +  engineName + " is " + engine.getResult() + ".");
+        try {
+            if(args.length==0){
+                throw new InvalidCommandException();
+            }
+            // Converts the first argument to uppercase to match the engine names.
+            String engineName = args[0].toUpperCase();
+    
+            // Declares a variable to hold the computable engine.
+            Computable engine = null;
+    
+            // Chooses the appropriate engine based on the specified engine name.
+            switch(engineName) {
+                case "LCM":
+                    engine = new LCMEngine();
+                    break;
+                case "GCD":
+                    engine = new GCDEngine();
+                    break;
+                case "SQRT":
+                    engine = new SQRTEngine();
+                    break;
+                case "FACTORIAL":
+                    engine = new FactorialEngine();
+                    break;
+                case "FIBONACCI":
+                    engine = new FibonacciEngine();
+                    break;
+                case "MAX":
+                    engine = new MaxEngine();
+                    break;
+                case "MIN":
+                    engine = new MinEngine();
+                    break;
+                case "CUBEVOL":
+                    engine = new CubeVolEngine();
+                    break;
+                case "SPHEREVOL":
+                    engine = new SphereVolEngine();
+                    break;
+                default:
+                    throw new InvalidCommandException(engineName);
+            }
+    
+            // Sets the input values for the engine.
+            engine.setInput(args);
+            // Computes the result using the specified engine.
+            engine.compute();
+    
+            // Prints the result of the computation.
+            System.out.println("The result of " +  engineName + " is " + engine.getResult() + ".");
+        
+        }catch(InvalidCommandException e){
+            System.out.println(e.getMessage());
+            System.exit(0);
+        } 
     }
 }
--- ASSIGNMENTS/HW6/MathCalculator/src/edu/handong/csee/java/hw6/exceptions/InvalidCommandException.java
+++ ASSIGNMENTS/HW6/MathCalculator/src/edu/handong/csee/java/hw6/exceptions/InvalidCommandException.java
@@ -3,11 +3,26 @@ package edu.handong.csee.java.hw6.exceptions;
 public class InvalidCommandException extends Exception{
     /**This is Constructor */
     public InvalidCommandException(){
-        super("Exception-01: Invalid command:\nPlease put a computing engine option such as LCM, GCD, SQRT, FACTORIAL, FIBONACCI, MAX, MIN, CUBEVOL, and SPHEREVOL. For example, ./app  MAX 12 4 5 45 100");
+        super(getString());
     }
 
     public InvalidCommandException(String function){
-        super("Exception-01: Invalid command: "+function+"/nPlease put a computing engine option such as LCM, GCD, SQRT, FACTORIAL, FIBONACCI, MAX, MIN, CUBEVOL, and SPHEREVOL. For example, ./app  MAX 12 4 5 45 100");
+        super(getString(function));
     }
 
+    public static String getString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Exception-01: Invalid command:\n");
+        sb.append("Please put a computing engine option such as LCM, GCD, SQRT, FACTORIAL, FIBONACCI, MAX, MIN, CUBEVOL, and SPHEREVOL. For example, ./app  MAX 12 4 5 45 100");
+        return sb.toString();
+    }
+
+    public static String getString(String function){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Exception-01: Invalid command: ");
+        sb.append(function+"\n");
+        
+        sb.append("Please put a computing engine option such as LCM, GCD, SQRT, FACTORIAL, FIBONACCI, MAX, MIN, CUBEVOL, and SPHEREVOL. For example, ./app  MAX 12 4 5 45 100");
+        return sb.toString();
+    }
 }
